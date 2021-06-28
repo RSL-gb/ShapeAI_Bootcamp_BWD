a28e2984305927498f09f3a5d934beb01c7e3c66
RSL-gb <79073022+RSL-gb@users.noreply.github.com>
2021-06-28 16:08:48 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and react.js</h1>
+      <p>a basic web development bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
